e2cb1f0c086c626c0941fdcefd4efee58d78f6f2
Mikhail Makashyn <mmakashyn@gmail.com>
2018-10-16 16:39:25 +0300

    for pull

---
+++ src/tables.py
@@ -0,0 +1,15 @@
+
+from models import CharField, IntegerField
+from basic_method import Model
+
+class Table1(Model):
+
+    name = CharField(default="asdf")
+    age = IntegerField(default=1)
+
+    def __str__(self):
+        return str(self.name) + "  " + str(self.age)
+
+
+
+
